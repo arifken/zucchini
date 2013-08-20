dc3ab36188c4917f5ab9542fbf2396b4f66938f5
Andy Rifken <andy.rifken@shoutlet.com>
2013-08-20 11:17:34 -0500

    Add screen directories for devices in config.yml when adding a new feature

--- lib/zucchini/generator.rb
+++ lib/zucchini/generator.rb
@@ -12,7 +12,20 @@ class Zucchini::Generator < Clamp::Command
     if project?
       FileUtils.mkdir_p(path)
       FileUtils.cp_r("#{templates_path}/project/.", path)
-    elsif feature? then FileUtils.cp_r("#{templates_path}/feature", path)
+    elsif feature?
+      Zucchini::Config.base_path = File.dirname(File.expand_path(path))
+      FileUtils.cp_r("#{templates_path}/feature", path)
+
+      # Create directories for the device screens listed in the config
+      screens = Zucchini::Config.devices.values.collect { |d| d['screen'] }.uniq
+      screens.each do |s|
+        %w(masks pending reference).each do |dir|
+          FileUtils.mkdir_p(File.join(path,dir,s))
+          FileUtils.touch(File.join(path,dir,s,'.gitkeep'))
+        end
+      end
+
     end
   end
+
 end
